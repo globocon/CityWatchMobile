93ce6edc1ba014c7642aa5e737c16afdb50a87fb
c4isupport <globoconsoftware@gmail.com>
2025-10-23 18:08:10 +1100

    version changes Done

--- C4iSytemsMobApp/LoginPage.xaml.cs
+++ C4iSytemsMobApp/LoginPage.xaml.cs
@@ -182,7 +182,7 @@ public partial class LoginPage : ContentPage
     private string GetAppVersion()
     {
         var version = Assembly.GetExecutingAssembly().GetName().Version;
-        return version?.ToString() ?? "1.24.0";
+        return version?.ToString() ?? "1.25.0";
     }
 
 
